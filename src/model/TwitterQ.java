e58affe1eae6e670761f53995e56f6fe3940421b
Josh Riccio <jriccio@email.arizona.edu>
2016-01-23 16:09:10 -0700

    add second paramater to colletdata()

--- src/model/TwitterQ.java
+++ src/model/TwitterQ.java
@@ -7,15 +7,10 @@ public class TwitterQ {
 	public static void main(String[] args) throws TwitterException {
 
 		ArrayList<MapData> mdlist = new ArrayList<MapData>();
-
 		MapData.collectData(mdlist, "");
-
 		for (MapData m : mdlist) {
 			System.out.println("Lat: " + m.getLat() + ", Long: " + m.getLong());
 		}
-		
-		
-
 	}
 	
 	
